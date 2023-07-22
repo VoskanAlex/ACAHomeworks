5c7da61e141ade4121493b319049862b2eb6e3e7
Alex Voskanyan <voskanalex@gmail.com>
2023-07-22 14:11:17 +0400

    first homework

---
+++ ComplexNumber.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplexNumbers
+{
+    public class ComplexNumber
+    {
+        public double Real { get; private set; }
+        public double Complex { get; private set; }
+
+        public ComplexNumber(double real, double complex)
+        {
+            Real = real;
+            Complex = complex;
+        }
+
+        public double AbsoluteValue()
+        {
+            return Math.Sqrt(Real * Real + Complex * Complex);
+        }
+
+        public static ComplexNumber operator +(ComplexNumber num1, ComplexNumber num2)
+        {
+            double realSum = num1.Real + num2.Real;
+            double complexSum = num1.Complex + num2.Complex;
+            return new ComplexNumber(realSum, complexSum);
+        }
+
+        public static ComplexNumber operator -(ComplexNumber num1, ComplexNumber num2)
+        {
+            double realDiff = num1.Real - num2.Real;
+            double complexDiff = num1.Complex - num2.Complex;
+            return new ComplexNumber(realDiff, complexDiff);
+        }
+
+        public static ComplexNumber operator *(ComplexNumber num, double scalar)
+        {
+            double realResult = num.Real * scalar;
+            double complexResult = num.Complex * scalar;
+            return new ComplexNumber(realResult, complexResult);
+        }
+
+        public static ComplexNumber operator *(ComplexNumber num1, ComplexNumber num2)
+        {
+            double realProduct = num1.Real * num2.Real - num1.Complex * num2.Complex;
+            double complexProduct = num1.Real * num2.Complex + num1.Complex * num2.Real;
+            return new ComplexNumber(realProduct, complexProduct);
+        }
+
+        public static implicit operator double(ComplexNumber num)
+        {
+            return num.Real;
+        }
+
+        public static explicit operator ComplexNumber(double real)
+        {
+            return new ComplexNumber(real, 0);
+        }
+
+        public override string ToString()
+        {
+            if (Complex >= 0)
+                return $"{Real} + {Complex}i";
+            else
+                return $"{Real} - {Math.Abs(Complex)}i";
+        }
+    }
+}
---
+++ MyStringBuilder.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomStringBuilder
+{
+    public class MyStringBuilder
+    {
+        private char[] stringArray;
+
+        public MyStringBuilder()
+        {
+            stringArray = new char[0];
+        }
+
+        public int Length => stringArray.Length;
+
+        public void Append(string s)
+        {
+            char[] sArray = s.ToCharArray();
+            int originalLength = stringArray.Length;
+            Array.Resize(ref stringArray, originalLength + sArray.Length);
+            Array.Copy(sArray, 0, stringArray, originalLength, sArray.Length);
+        }
+
+        public void InsertAt(int index, string s)
+        {
+            if (index < 0 || index > stringArray.Length)
+            {
+                throw new IndexOutOfRangeException("Invalid index.");
+            }
+
+            char[] sArray = s.ToCharArray();
+            int originalLength = stringArray.Length;
+            Array.Resize(ref stringArray, originalLength + sArray.Length);
+            Array.Copy(stringArray, index, stringArray, index + sArray.Length, originalLength - index);
+            Array.Copy(sArray, 0, stringArray, index, sArray.Length);
+        }
+
+        public void RemoveDuplicates()
+        {
+            int tail = 0;
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                int j;
+                for (j = 0; j < tail; j++)
+                {
+                    if (stringArray[i] == stringArray[j])
+                        break;
+                }
+                if (j == tail)
+                {
+                    stringArray[tail] = stringArray[i];
+                    tail++;
+                }
+            }
+            Array.Resize(ref stringArray, tail);
+        }
+
+        public void RemoveWhitespaces()
+        {
+            string modifiedString = new string(stringArray);
+            modifiedString = modifiedString.Replace(" ", string.Empty);
+            modifiedString = modifiedString.Replace("\t", string.Empty);
+            modifiedString = modifiedString.Replace("\n", string.Empty);
+            modifiedString = modifiedString.Replace("\r", string.Empty);
+            stringArray = modifiedString.ToCharArray();
+        }
+
+        public string GetString()
+        {
+            return new string(stringArray);
+        }
+
+        public bool IsBlank()
+        {
+            if (stringArray.Length == 0)
+            {
+                return true;
+            }
+
+            foreach (char c in stringArray)
+            {
+                if (!char.IsWhiteSpace(c))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public string OnBlank(string defaultString)
+        {
+            if (IsBlank())
+            {
+                return defaultString;
+            }
+
+            return GetString();
+        }
+
+    }
+}
